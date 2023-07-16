3cf513cccf84f3e8b9ac9d5ff9e7f6301aacb8eb
sathvikmuppa <sathvikmuppa@gmail.com>
2023-07-16 16:52:12 -0500

    continuing implementing corner resizing

--- Window.java
+++ Window.java
@@ -25,6 +25,9 @@ public class Window extends JFrame implements ActionListener {
     int mouseX;
     int mouseY;
     GraphicsDevice device = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+    // GraphicsDevice device =
+    // GraphicsEnvironment.getLocalGraphicsEnvironment().getScreenDevices()[1];
+
     JButton temp;
     ImageIcon maxIcon;
     ImageIcon normIcon;
@@ -49,15 +52,15 @@ public class Window extends JFrame implements ActionListener {
 
         this.addMouseMotionListener(new MouseMotionAdapter() {
             public void mouseMoved(MouseEvent e) {
-                if (e.getX() <=10 && e.getY() >= Window.this.getHeight()-10) {
+                if (e.getX() <= 10 && e.getY() >= Window.this.getHeight() - 10) {
                     Window.this.setCursor(new Cursor(Cursor.NE_RESIZE_CURSOR));
-                } else if (e.getX()>=Window.this.getWidth()-10 && e.getY() >= Window.this.getHeight()-10) {
+                } else if (e.getX() >= Window.this.getWidth() - 10 && e.getY() >= Window.this.getHeight() - 10) {
                     Window.this.setCursor(new Cursor(Cursor.NW_RESIZE_CURSOR));
                 } else if (e.getX() <= 5) {
                     Window.this.setCursor(new Cursor(Cursor.W_RESIZE_CURSOR));
-                } else if (e.getX() >= Window.this.getWidth()-5){
+                } else if (e.getX() >= Window.this.getWidth() - 5) {
                     Window.this.setCursor(new Cursor(Cursor.E_RESIZE_CURSOR));
-                } else if (e.getY() >= Window.this.getHeight()-5) {
+                } else if (e.getY() >= Window.this.getHeight() - 5) {
                     Window.this.setCursor(new Cursor(Cursor.S_RESIZE_CURSOR));
                 } else {
                     Window.this.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
@@ -65,12 +68,17 @@ public class Window extends JFrame implements ActionListener {
             }
 
             public void mouseDragged(MouseEvent e) {
-                if (Window.this.getCursor().getType() == Cursor.W_RESIZE_CURSOR) {
-                    if (Window.this.getWidth()!=Window.this.getMinimumSize().getWidth()) {
+                if (Window.this.getCursor().getType() == Cursor.NE_RESIZE_CURSOR) {
+
+                } else if (Window.this.getCursor().getType() == Cursor.NW_RESIZE_CURSOR) {
+                    Window.this.setSize(e.getX(), e.getY());
+                } else if (Window.this.getCursor().getType() == Cursor.W_RESIZE_CURSOR) {
+                    if (Window.this.getWidth() != Window.this.getMinimumSize().getWidth()) {
                         if (Window.this.getWidth() - e.getX() > Window.this.getMinimumSize().getWidth()) {
                             Window.this.setLocation(Window.this.getX() + e.getX(), Window.this.getY());
                         } else {
-                            Window.this.setLocation((int) (Window.this.getX() + Window.this.getWidth() - Window.this.getMinimumSize().getWidth()), Window.this.getY());
+                            Window.this.setLocation((int) (Window.this.getX() + Window.this.getWidth()
+                                    - Window.this.getMinimumSize().getWidth()), Window.this.getY());
                         }
                     }
                     Window.this.setSize(Window.this.getWidth() - e.getX(), Window.this.getHeight());
@@ -97,8 +105,8 @@ public class Window extends JFrame implements ActionListener {
             }
 
             public void mouseClicked(MouseEvent e) {
-                if (e.getClickCount()==2) {
-                    if (Window.this.getExtendedState()!=JFrame.MAXIMIZED_BOTH) {
+                if (e.getClickCount() == 2) {
+                    if (Window.this.getExtendedState() != JFrame.MAXIMIZED_BOTH) {
                         Window.this.setExtendedState(JFrame.MAXIMIZED_BOTH);
                     } else {
                         Window.this.setExtendedState(JFrame.NORMAL);
@@ -112,33 +120,40 @@ public class Window extends JFrame implements ActionListener {
                 if (device.getFullScreenWindow() == null) {
                     if (Window.this.getCursor().getType() == Cursor.DEFAULT_CURSOR) {
                         Window.this.setLocation(Window.this.getX() + e.getX() - mouseX,
-                        Window.this.getY() + e.getY() - mouseY);
+                                Window.this.getY() + e.getY() - mouseY);
                     } else if (Window.this.getCursor().getType() == Cursor.N_RESIZE_CURSOR) {
-                        if (Window.this.getHeight()!=Window.this.getMinimumSize().getHeight()) {
+                        if (Window.this.getHeight() != Window.this.getMinimumSize().getHeight()) {
                             if (Window.this.getHeight() - e.getY() > Window.this.getMinimumSize().getHeight()) {
                                 Window.this.setLocation(Window.this.getX(), Window.this.getY() + e.getY());
                             } else {
-                                Window.this.setLocation(Window.this.getX(), (int) (Window.this.getY() + Window.this.getHeight() - Window.this.getMinimumSize().getHeight()));
+                                Window.this.setLocation(Window.this.getX(), (int) (Window.this.getY()
+                                        + Window.this.getHeight() - Window.this.getMinimumSize().getHeight()));
                             }
                         }
                         Window.this.setSize(Window.this.getWidth(), Window.this.getHeight() - e.getY());
                     } else if (Window.this.getCursor().getType() == Cursor.SE_RESIZE_CURSOR) {
-                        if (Window.this.getWidth()!=Window.this.getMinimumSize().getWidth() && Window.this.getHeight()!=Window.this.getMinimumSize().getHeight()) {
-                            if (Window.this.getWidth() - e.getX() > Window.this.getMinimumSize().getWidth() && Window.this.getHeight() - e.getY() > Window.this.getMinimumSize().getHeight()) {
+                        if (Window.this.getWidth() != Window.this.getMinimumSize().getWidth()
+                                && Window.this.getHeight() != Window.this.getMinimumSize().getHeight()) {
+                            if (Window.this.getWidth() - e.getX() > Window.this.getMinimumSize().getWidth()
+                                    && Window.this.getHeight() - e.getY() > Window.this.getMinimumSize().getHeight()) {
                                 Window.this.setLocation(Window.this.getX() + e.getY(), Window.this.getY() + e.getY());
                             } else {
-                                Window.this.setLocation((int) (Window.this.getX() + Window.this.getWidth() - Window.this.getMinimumSize().getWidth()), (int) (Window.this.getY() + Window.this.getHeight() - Window.this.getMinimumSize().getHeight()));
+                                Window.this.setLocation(
+                                        (int) (Window.this.getX() + Window.this.getWidth()
+                                                - Window.this.getMinimumSize().getWidth()),
+                                        (int) (Window.this.getY() + Window.this.getHeight()
+                                                - Window.this.getMinimumSize().getHeight()));
                             }
                         }
-                        Window.this.setSize(Window.this.getWidth(), Window.this.getHeight() - e.getY());                 
+                        Window.this.setSize(Window.this.getWidth(), Window.this.getHeight() - e.getY());
                     }
                 }
             }
 
             public void mouseMoved(MouseEvent e) {
-                if (e.getX() <=10 && e.getY() <= 10) {
+                if (e.getX() <= 10 && e.getY() <= 10) {
                     Window.this.setCursor(new Cursor(Cursor.SE_RESIZE_CURSOR));
-                } else if (e.getX()>=Window.this.getWidth()-10 && e.getY() <= 10) {
+                } else if (e.getX() >= Window.this.getWidth() - 10 && e.getY() <= 10) {
                     Window.this.setCursor(new Cursor(Cursor.SW_RESIZE_CURSOR));
                 } else if (e.getY() <= 5) {
                     Window.this.setCursor(new Cursor(Cursor.N_RESIZE_CURSOR));
@@ -199,10 +214,14 @@ public class Window extends JFrame implements ActionListener {
                     if (device.getFullScreenWindow() == null) {
                         device.setFullScreenWindow(this);
                         max.setIcon(normIcon);
+                        min.setEnabled(false);
+                        min.removeMouseListener(ma);
                         this.setVisible(true);
                     } else {
                         device.setFullScreenWindow(null);
                         max.setIcon(maxIcon);
+                        min.setEnabled(true);
+                        min.addMouseListener(ma);
                         this.setVisible(true);
                     }
                 } catch (Exception ex) {
